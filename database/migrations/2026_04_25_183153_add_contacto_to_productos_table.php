0057598224e134b601f41d429f892e42ec12c9c9
Juan Otero <champion107411@gmail.com>
2026-04-25 13:42:07 -0500

    Se borro una migracion y se agrego la tabla contacto a la base de datos

--- database/migrations/2026_04_25_183153_add_contacto_to_productos_table.php
+++ database/migrations/2026_04_25_183153_add_contacto_to_productos_table.php
@@ -11,8 +11,8 @@ return new class extends Migration
      */
     public function up(): void
     {
-       Schema::table('users', function (Blueprint $table) {
-    $table->enum('rol', ['admin','estudiante'])->default('estudiante');
+        Schema::table('productos', function (Blueprint $table) {
+    $table->string('contacto')->nullable();
 });
     }
 
@@ -21,7 +21,7 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::table('users', function (Blueprint $table) {
+        Schema::table('productos', function (Blueprint $table) {
             //
         });
     }
